a91926cb05c5ac20e76ed71d01486d7a06290912
rd102 <sugarwl@163.com>
2017-02-22 14:05:49 +0800

    wrote a readme file

---
+++ exc_6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+
+{
+	int i,j,k;
+	char *tem;
+	char *name[5] = {"follow","basic","great","happy","computer"};
+
+	for(i = 0;i < 5-1;i++)
+	{
+		k = i;
+		for(j= i+1; j <5;j++)
+		{
+			if(strcmp(name[k],name[j]) > 0)
+				k = j;
+		}	
+		if(k != i)
+		{
+			tem = name[i];
+			name[i] = name[k];
+			name[k] = tem;
+		}
+	}
+
+	for(i = 0;i < 5;i++)
+		puts(name[i]);
+	exit(0);      
+
+	
+} 
